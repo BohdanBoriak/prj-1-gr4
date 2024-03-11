1531066088f93d2b5bacd31edf3d653df87c65e8
BohdanBoriak <lightraven72@gmail.com>
2024-03-11 08:45:46 +0200

    Added game functionality

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var points int = 20
+
+const pointsPerQuestion = 5
 
 func main() {
-	fmt.Println("Привіт, World!")
+	fmt.Println("Привіт, вітаємо у грі!")
+	time.Sleep(1 * time.Second)
+
+	for i := 5; i > 1; i-- {
+		fmt.Printf("До початку: %v", i)
+		time.Sleep(1 * time.Second)
+	}
+
+	myPoints := 0
+	start := time.Now()
+	for points > 0 {
+		x, y := rand.Intn(100), rand.Intn(100)
+		res := x + y
+		fmt.Printf("%v + %v = ", x, y)
+
+		ans := ""
+		fmt.Scan(&ans)
+
+		ansInt, err := strconv.Atoi(ans)
+		if err != nil {
+			fmt.Println("Спробуй ще!")
+		} else {
+			if res == ansInt {
+				myPoints += pointsPerQuestion
+				points -= pointsPerQuestion
+				fmt.Println("Правильно! Ура!")
+				fmt.Printf("У тебе балів: %v, залишилось зібрати: %v\n", myPoints, points)
+			} else {
+				fmt.Println("Палюча поразка! Вчи математику!")
+			}
+		}
+	}
+	end := time.Now()
+	timeSpent := end.Sub(start)
+
+	fmt.Printf("Вітаю, ти впорався за %v", timeSpent)
+	time.Sleep(10 * time.Second)
 }
