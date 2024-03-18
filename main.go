a3ed0cbac88adb9bd2dc9acc81aaa76c9116df73
BohdanBoriak <lightraven72@gmail.com>
2024-03-18 09:57:22 +0200

    Added: storing users to json

--- main.go
+++ main.go
@@ -1,45 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
+	"os"
 	"prj-1-gr4/domain"
+	"sort"
 	"strconv"
 	"time"
 )
 
-var (
-	id     uint64 = 1
-	points int    = 20
-)
+var id uint64 = 1
 
-const pointsPerQuestion = 5
+const (
+	pointsPerQuestion = 20
+	totalPoints       = 20
+)
 
 func main() {
 	fmt.Println("Привіт, вітаємо у грі!")
 	time.Sleep(1 * time.Second)
 
 	var users []domain.User
+	users = append(users, domain.User{Id: 1, Name: "Vasyl", Time: 5 * time.Second})
+	users = append(users, domain.User{Id: 2, Name: "Mykola", Time: 3 * time.Second})
+	users = append(users, domain.User{Id: 3, Name: "Artem", Time: 10 * time.Second})
 
-	for {
-		menu()
-		punct := ""
-		fmt.Scan(&punct)
+	sortAndSave(users)
+	// for {
+	// 	menu()
+	// 	punct := ""
+	// 	fmt.Scan(&punct)
 
-		switch punct {
-		case "1":
-			u := play()
-			users = append(users, u)
-		case "2":
-			for _, user := range users {
-				fmt.Printf("Name: %s, Time: %v", user.Name, user.Time)
-			}
-		case "3":
-			return
-		default:
-			fmt.Println("Зробіть правильний вибір")
-		}
-	}
+	// 	switch punct {
+	// 	case "1":
+	// 		u := play()
+	// 		users = append(users, u)
+	// 	case "2":
+	// 		for _, user := range users {
+	// 			fmt.Printf("Name: %s, Time: %v\n", user.Name, user.Time)
+	// 		}
+	// 	case "3":
+	// 		return
+	// 	default:
+	// 		fmt.Println("Зробіть правильний вибір")
+	// 	}
+	// }
 }
 
 func menu() {
@@ -50,11 +57,12 @@ func menu() {
 
 func play() domain.User {
 	for i := 5; i > 1; i-- {
-		fmt.Printf("До початку: %v", i)
+		fmt.Printf("До початку: %v\n", i)
 		time.Sleep(1 * time.Second)
 	}
 
 	myPoints := 0
+	points := totalPoints
 	start := time.Now()
 	for points > 0 {
 		x, y := rand.Intn(100), rand.Intn(100)
@@ -81,7 +89,7 @@ func play() domain.User {
 	end := time.Now()
 	timeSpent := end.Sub(start)
 
-	fmt.Printf("Вітаю, ти впорався за %v", timeSpent)
+	fmt.Printf("Вітаю, ти впорався за %v\n", timeSpent)
 
 	fmt.Println("Введіть своє ім'я:")
 
@@ -96,4 +104,30 @@ func play() domain.User {
 	id++
 
 	return user
+}
+
+func sortAndSave(users []domain.User) {
+	sort.SliceStable(users, func(i, j int) bool {
+		return users[i].Time < users[j].Time
+	})
+
+	file, err := os.OpenFile("users.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+
+	defer func(file *os.File) {
+		err = file.Close()
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+		}
+	}(file)
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(users)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
 }
